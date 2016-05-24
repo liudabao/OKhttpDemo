afc237ce48e48d037f081ee9a00213352172ab55
liudabao <15810163587@163.com>
2016-05-24 23:08:48 +0800

    second commit

--- app/src/main/java/com/example/lenovo/okhttpdemo/MainActivity.java
+++ app/src/main/java/com/example/lenovo/okhttpdemo/MainActivity.java
@@ -7,11 +7,20 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.gson.Gson;
+import com.squareup.okhttp.Call;
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.FormEncodingBuilder;
+import com.squareup.okhttp.Headers;
+import com.squareup.okhttp.MediaType;
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
 import com.squareup.okhttp.Response;
+import com.squareup.okhttp.internal.spdy.Header;
 
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -42,7 +51,43 @@ public class MainActivity extends AppCompatActivity {
                 new Thread(new Runnable() {
                     @Override
                     public void run() {
-                        get("https://publicobject.com/helloworld.txt");
+                        getSync("https://publicobject.com/helloworld.txt");
+                    }
+                }).start();
+
+            }
+        });
+        bt2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        getAsync("https://publicobject.com/helloworld.txt");
+                    }
+                }).start();
+
+            }
+        });
+        bt3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        postSync("https://www.baidu.com");
+                    }
+                }).start();
+
+            }
+        });
+        bt4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        postAsync("https://www.baidu.com");
                     }
                 }).start();
 
@@ -50,18 +95,95 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private void get(String url){
-
+    private void getSync(String url){
+        client.setConnectTimeout(5, TimeUnit.SECONDS);
+        client.setReadTimeout(5, TimeUnit.SECONDS);
+        client.setWriteTimeout(5, TimeUnit.SECONDS);
         Request request=new Request.Builder().url(url).build();
         try {
             Response response=client.newCall(request).execute();
             if(response.isSuccessful()){
-                Log.e("Get","ok");
+                Log.e("Get","sync ok "+response.body().string());
+                Headers header=response.headers();
+                for(int i=0;i<header.size();i++){
+                    Log.e("Header",header.name(i)+" "+header.value(i));
+                }
+
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
+    private void getAsync(String url){
+        Request request=new Request.Builder().url(url).build();
+        Call call=client.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Request request, IOException e) {
+                Log.e("Get","async failed");
+            }
+
+            @Override
+            public void onResponse(Response response) throws IOException {
+                if(response.isSuccessful()){
+                    Log.e("Get","async succeed "+response.body().string());
+                }
+                else {
+                    Log.e("Get","async response failed");
+                }
+
+            }
+        });
+       // call.cancel();
+
+    }
+
+    private void postSync(String url){
+        RequestBody body=new FormEncodingBuilder()
+                .add("name","test")
+                .add("psd","123456")
+                .build();
+        Request request=new Request.Builder().url(url).post(body).build();
+        try {
+            Response response=client.newCall(request).execute();
+            if(response.isSuccessful()){
+                Log.e("Post","sync ok ");
+            }
+            else {
+                Log.e("Post","sync failed ");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void postAsync(String url){
+        MediaType JSON=MediaType.parse("application/json;charset=utf-8");
+        Person person=new Person();
+        person.setId(1);
+        person.setName("test");
+        Gson gson=new Gson();
+        String json=gson.toJson(person);
+        Log.e("JSON", json);
+        RequestBody body=RequestBody.create(JSON,json);
+        Request request=new Request.Builder().url(url).post(body).build();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Request request, IOException e) {
+                Log.e("Post","async ok ");
+            }
+
+            @Override
+            public void onResponse(Response response) throws IOException {
+                Log.e("Post","async failed ");
+            }
+        });
+
+    }
+
+
 
 
 }
